24744c7a49bf0935184a4a497ab5c1fdaae2a050
Vlad Lazar <vladlazar.25042000@gmail.com>
2024-06-17 21:44:35 +0200

    aggiunto scaffolding base, fortify, layout e navbar

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <title>{{env('APP_NAME')}}</title>
+    </head>
+    <body>
+        <x-navbar/>
+        <div class="container">
+        {{$slot}}
+        </div>
+    </body>
+    </html>
+</div>
