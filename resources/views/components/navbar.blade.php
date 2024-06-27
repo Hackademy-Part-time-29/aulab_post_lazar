201d7637cb21cba82c96bef1c1d506d4107cf198
Fiorusss <giordano.fioretti1993@gmail.com>
2024-06-27 21:25:26 +0200

    aggiustato un po' il frontend

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div>
 <nav class="navbar navbar-expand-lg fixed-top" id="navbarJS">
   <div class="container-fluid">
-    <a class="navbar-brand d-none" href="{{route('homepage')}}">{{env('APP_NAME')}}</a>
+    <a class="navbar-brand" href="{{route('homepage')}}"><img class="logo" src= "/img/logo/logo2.png" alt="{{env('APP_NAME')}}"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,7 +14,7 @@
           <a class="nav-link" href="{{route('article.index')}}">Articoli</a>
         </li>
         @auth
-        @if(Auth::user()->is_admin)
+        @if(Auth::user()->is_admin)++
         <li class="nav-item">
           <a class= "nav-link" href="{{route ('admin.dashboard')}}">Dashboard amministratori</a>
         </li>
@@ -28,7 +28,7 @@
           <a class="nav-link" href="{{route('article.create')}}">Inserisci articolo</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('careers')}}">Lavora con noi</a>
+          <a class="nav-link" aria-current="page" href="{{route('careers')}}">Lavora con noi</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
