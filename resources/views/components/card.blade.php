1b7833822223f2022e0bf9591f8178eb3424ddf9
pierpaolo calafato <piercal@hotmail.it>
2024-07-03 20:39:28 +0200

    modifiche css

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,10 +1,11 @@
 <div>
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 18rem; height:36rem;">
   <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{$article->title}}</h5>
     <h6 class="card-text">{{$article->subtitle}}</h6>
     <p class="card-text">{{Str::limit($article->body, 33)}}</p>
+    <hr>
     <a href="{{route('article.show', $article)}}" class="btn btn-primary">Leggi l'articolo</a>
     @if($article->category)
     <p class="small text-muted my-2">Categoria: <br>
